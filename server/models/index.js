41774f28bc58a1cd8618b3cdbb0d48932e599585
sethonne <20103261@usc.edu.ph>
2023-05-17 10:35:45 +0800

    initial commit

--- server/models/index.js
+++ server/models/index.js
@@ -14,14 +14,12 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize({
-    username: "admin",
-    password: "Sf1EZ7vN",
-    database: "acta",
-    host: "mysql-126667-0.cloudclusters.net",
-    port: 10041,
-    dialect: "mysql",
-  });
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs.readdirSync(__dirname)
